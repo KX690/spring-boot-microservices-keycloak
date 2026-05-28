da22be09c0a9a46d97f3a02ae225275a354cf2a6
Kevin Lezcano <klezcano@excelsis.com.py>
2026-05-28 14:54:01 -0300

    First commit

---
+++ products-service/src/main/java/com/uncledavecode/products_service/model/dtos/ProductResponse.java
@@ -0,0 +1,17 @@
+package com.uncledavecode.products_service.model.dtos;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductResponse {
+
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean status;
+}
